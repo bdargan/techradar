62c9b5c0d7af4f5d4c1e38ce858e0ee93bd10b31
Stephen Bailey <Stephen.anthony.Bailey@gmail.com>
2013-07-10 21:21:45 +0200

    Making more generic
    Attempting to make this slightly more generic in nature so that it can
    be used for different types of radars.
    
    The radar.html is now generic, and assumes that you are using the
    radarData.js file ( the expectation is that you will simply rename the
    data file you want to now reference. The goals is to change the fewest
    things possible ).
    
    The title for the html page is now driven from the radarData.js
    
    The radar arcs is now no longer hard coded to 4, but can be N , and the
    size and contents is determined by the radar_arcs now in RadarData.js
    
    The data is now able to determine the size of the blip to allow users to
    use the size of the blip to  relay extra information.
    
    Each Blip can now have an optional URL associated with it, and if it
    does the cursor will turn into a pointer.
    
    Removed unused code to clean up a bit.

--- radar.js
+++ radar.js
@@ -1,8 +1,6 @@
 function init(h,w) {
-  $('#title').text(radar_title);
-
-  var radar_arcs = [{'r':100,'name':'Adopt'},{'r':200,'name':'Trial'},{'r':300,'name':'Assess'},{'r':400,'name':'Hold'}];
-  
+  $('#title').text(document.title);  
+	   
  var radar = new pv.Panel()
       .width(w)
       .height(h)
@@ -18,36 +16,47 @@ radar.add(pv.Dot)
        .anchor("top")       
        .add(pv.Label).text(function(d) { return d.name;});
 
+//quadrant lines -- vertical
+radar.add(pv.Line)
+        .data([(h/2-radar_arcs[radar_arcs.length-1].r),h-(h/2-radar_arcs[radar_arcs.length-1].r)])
+        .lineWidth(1)
+        .left(w/2)        
+        .bottom(function(d) {return d;})       
+        .strokeStyle("#bbb");
+
+//quadrant lines -- horizontal 
+radar.add(pv.Line)
+        .data([(w/2-radar_arcs[radar_arcs.length-1].r),w-(w/2-radar_arcs[radar_arcs.length-1].r)])
+        .lineWidth(1)
+        .bottom(h/2)
+        .left(function(d) {return d;})       
+        .strokeStyle("#bbb");
+
+
 // blips
 var total_index=1;
 for (var i = 0; i < radar_data.length; i++) {
-  radar.add(pv.Dot)
-       .size(70)
-       .def("active", false)
-       .data(radar_data[i].items)
-       .left(function(d) { var x = polar_to_raster(d.pc.r, d.pc.t)[0];
-              console.log("name:" + d.name + ", x:" + x); return x;})
-         .bottom(function(d) { var y = polar_to_raster(d.pc.r, d.pc.t)[1];                                 
-                  console.log("name:" + d.name + ", y:" + y); return y;})
-         .title(function(d) { return d.name;})
-         .angle(45)
-         .strokeStyle(radar_data[i].color)
-         .fillStyle(radar_data[i].color)
-         .shape(function(d) {return (d.movement === 't' ? "triangle" : "circle");})
-         // .event("mouseover", function() {this.fillStyle("orange");})
-         // .event("mouseout", function() {this.fillStyle("#aec7e8");})         
-         .anchor("center").add(pv.Label)
-             .text(function(d) {return total_index++;}) 
-             .textBaseline("middle")
-             .textStyle("white");
-
-             // .fillStyle("#aec7e8")
-
-             // .add(pv.Panel)
-             //   .def("active", false)
-             // .fillStyle(function() {this.parent.active() ? "orange" : "#aec7e8";})
-             //   .event("mouseover", function() {this.parent.active(true);})
-             //   .event("mouseout", function() {this.parent.active(false);})         
+    radar.add(pv.Dot)       
+    .def("active", false)
+    .data(radar_data[i].items)
+    .size( function(d) { return ( d.blipSize !== undefined ? d.blipSize : 70 ); })
+    .left(function(d) { var x = polar_to_raster(d.pc.r, d.pc.t)[0];
+                        console.log("name:" + d.name + ", x:" + x); 
+                        return x;})
+    .bottom(function(d) { var y = polar_to_raster(d.pc.r, d.pc.t)[1];                                 
+                          console.log("name:" + d.name + ", y:" + y); 
+                          return y;})
+    .title(function(d) { return d.name;})		 
+    .cursor( function(d) { return ( d.url !== undefined ? "pointer" : "auto" ); })                                                            
+    .event("click", function(d) { if ( d.url !== undefined ){self.location =  d.url}}) 
+    .angle(45)
+    .strokeStyle(radar_data[i].color)
+    .fillStyle(radar_data[i].color)
+    .shape(function(d) {return (d.movement === 't' ? "triangle" : "circle");})         
+    .anchor("center").add(pv.Label)
+        .text(function(d) {return total_index++;}) 
+        .textBaseline("middle")
+        .textStyle("white");            
 }
 
 
@@ -70,31 +79,18 @@ function draw_legend(quad, left, top) {
       .size(8) 
       .strokeStyle(null) 
       .fillStyle("#aec7e8") 
-    .anchor("right")
+      .anchor("right")
           .add(pv.Label)
           .text(function(d) {return (quad.start + 1 + this.index) + ". " + d.name;} );
 }
-/*
-//race conditions?
-for (var i = 0; i < radar_quadrants.length; i++) {
-  var qleft = 5 + (1020 * ((i+1) % 2));
-  var qtop = 36 + (500 * (i > 1 ? 1: 0));
-  var quad = radar_quadrants[i];
-  radar.add(pv.Dot)
-      .strokeStyle(null)
-      .left(5)
-  
-  draw_legend(quad, qleft, qtop);
-  
-} */            
 
 var radar_quadrant_ctr=1;
     radar.add(pv.Label)
          .left(45)
          .top(18)
          .fillStyle("#aec7e8") 
-         .text(radar_data[0].quadrant)
-         .font("18px sans-serif");
+         .text(radar_data[0].quadrant)		 
+         .font("18px sans-serif");		 
   
     radar.add(pv.Dot) 
         .data(radar_data[0].items) 
@@ -105,7 +101,8 @@ var radar_quadrant_ctr=1;
         .angle(45)
         .shape(function(d) {return (d.movement === 't' ? "triangle" : "circle");})        
         .fillStyle("#aec7e8") 
-      .anchor("right").add(pv.Label).text(function(d) {return radar_quadrant_ctr++ + ". " + d.name;} );
+        .anchor("right").add(pv.Label)						
+			.text(function(d) {return radar_quadrant_ctr++ + ". " + d.name;} );
 
   radar.anchor("left").add(pv.Label)
        .left(w-200+30)  
@@ -122,7 +119,7 @@ var radar_quadrant_ctr=1;
         .angle(45)
         .shape(function(d) {return (d.movement === 't' ? "triangle" : "circle");})         
         .strokeStyle(null) 
-        .fillStyle("#aec7e8")         
+        .fillStyle("#aec7e8")  				
         .anchor("right").add(pv.Label).text(function(d) {return radar_quadrant_ctr++ + ". " + d.name;} );
 
     radar.anchor("left").add(pv.Label)
@@ -161,21 +158,6 @@ var radar_quadrant_ctr=1;
         .fillStyle("#aec7e8") 
         .anchor("right").add(pv.Label).text(function(d) {return radar_quadrant_ctr++ + ". " + d.name;} );
 
-
-//quadrant lines
-radar.add(pv.Line)
-        .data([(h/2-radar_arcs[3].r),h-(h/2-radar_arcs[3].r)])
-        .lineWidth(1)
-        .left(w/2)        
-        .bottom(function(d) {return d;})       
-        .strokeStyle("#bbb");
-
-radar.add(pv.Line)
-                .data([200,w-200])
-                .lineWidth(1)
-                .bottom(h/2)
-                .left(function(d) {return d;})       
-                .strokeStyle("#bbb");
        
  radar.anchor('radar');
  radar.render();
