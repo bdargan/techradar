adb1dc3219ad4d0aec7905b902ae726f17671a75
Adam Londero <alondero@gmail.com>
2016-08-30 14:09:41 +0100

    #7 Adding new stuff to techradar

--- radars/radarData.js
+++ radars/radarData.js
@@ -56,12 +56,12 @@ var radar_data = [
         //  Adopt
         {"pc": {"r":  30, "t": 100}, "movement": "c", "name": "Spring Boot",}, 
         {"pc": {"r":  38, "t": 162}, "movement": "c", "name": "SASS",}, 
-        {"pc": {"r":  66, "t": 120}, "movement": "c", "name": "Swagger",},
         {"pc": {"r":  53, "t": 140}, "movement": "t", "name": "Gradle",},  
+        {"pc": {"r":  66, "t": 120}, "movement": "c", "name": "Swagger",},
         {"pc": {"r":  70, "t": 167}, "movement": "c", "name": "Thymeleaf",}, 
+        {"pc": {"r":  78, "t": 138}, "movement": "t", "name": "node.js (for micro-services or simple front-ends)",}, 
 
         //  Trial
-        {"pc": {"r": 108, "t": 138}, "movement": "c", "name": "node.js",}, 
         {"pc": {"r": 110, "t": 125}, "movement": "c", "name": "Kotlin",}, 
         {"pc": {"r": 132, "t": 139}, "movement": "c", "name": "Grunt",}, 
         {"pc": {"r": 150, "t": 159}, "movement": "c", "name": "React + Redux",}, 
@@ -69,21 +69,22 @@ var radar_data = [
 
         //  Assess
         {"pc": {"r": 210, "t": 115}, "movement": "t", "name": ".NET Core",}, 
-        {"pc": {"r": 247, "t": 120}, "movement": "c", "name": "TypeScript",}, 
-        {"pc": {"r": 212, "t": 140}, "movement": "c", "name": "Angular 2.0",}, 
+        {"pc": {"r": 217, "t": 120}, "movement": "c", "name": "TypeScript",}, 
+        {"pc": {"r": 222, "t": 140}, "movement": "c", "name": "Angular 2.0",}, 
+        {"pc": {"r": 259, "t": 132}, "movement": "t", "name": "Python",},
         {"pc": {"r": 274, "t": 154}, "movement": "c", "name": "Ember",}, 
-        {"pc": {"r": 280, "t": 143}, "movement": "c", "name": "Play Framework",},
         {"pc": {"r": 282, "t": 149}, "movement": "c", "name": "Knockout.js",},
         {"pc": {"r": 289, "t": 127}, "movement": "c", "name": "Akka",},
 
         //  Hold 
-        {"pc": {"r": 344, "t": 138}, "movement": "c", "name": "Python + Django",}, 
-        {"pc": {"r": 349, "t": 115}, "movement": "c", "name": ".NET",}, 
+        {"pc": {"r": 344, "t": 138}, "movement": "c", "name": "Django",}, 
+        {"pc": {"r": 349, "t": 115}, "movement": "c", "name": ".NET for new bespoke development",}, 
         {"pc": {"r": 370, "t": 108}, "movement": "c", "name": "Ruby-on-Rails",}, 
-        {"pc": {"r": 378, "t": 143}, "movement": "c", "name": "Go (lang)",}, 
+        {"pc": {"r": 372, "t": 143}, "movement": "t", "name": "Play Framework",},
+        {"pc": {"r": 378, "t": 133}, "movement": "c", "name": "Go (lang)",}, 
         {"pc": {"r": 388, "t": 125}, "movement": "c", "name": "Java EE",}, 
         {"pc": {"r": 390, "t":  98}, "movement": "c", "name": "Elixir + Phoenix",}, 
-        {"pc": {"r": 392, "t": 166}, "movement": "c", "name": "LESS",}, 
+        {"pc": {"r": 392, "t": 166}, "movement": "c", "name": "LESS (CSS)",}, 
     ]
 }, {
     "quadrant": "Platforms", // t 0-90
@@ -97,12 +98,14 @@ var radar_data = [
         {"pc": {"r":  83, "t":  61}, "movement": "c", "name": "Joomla!",},
         
         //  Trial
+        {"pc": {"r": 112, "t":  58}, "movement": "c", "name": "VSTS",}, 
         {"pc": {"r": 118, "t":  24}, "movement": "c", "name": "MongoDB",}, 
         {"pc": {"r": 158, "t":  21}, "movement": "c", "name": "Azure SQL",}, 
         {"pc": {"r": 188, "t":  31}, "movement": "c", "name": "AWS Lambda & Azure Functions",},
 
         //  Assess 
-        {"pc": {"r": 209, "t":  68}, "movement": "c", "name": "Powershell DSC",}, 
+        {"pc": {"r": 209, "t":  68}, "movement": "c", "name": "Powershell DSC",},
+        {"pc": {"r": 225, "t":  38}, "movement": "t", "name": "Cloud Foundry",}, 
         {"pc": {"r": 244, "t":  42}, "movement": "c", "name": "Amazon SNS & SQS",}, 
         {"pc": {"r": 274, "t":  14}, "movement": "c", "name": "Azure DocumentDB",}, 
         {"pc": {"r": 284, "t":  78}, "movement": "c", "name": "Windows Server Nano",}, 
@@ -126,7 +129,6 @@ var radar_data = [
         {"pc": {"r":  88, "t": 202}, "movement": "c", "name": "UXpin",}, 
 
         //  Trial
-        {"pc": {"r": 112, "t": 235}, "movement": "c", "name": "VSTS",}, 
         {"pc": {"r": 123, "t": 258}, "movement": "c", "name": "ELK Stack (ElasticSearch/Logstash/Kibana)",}, 
         {"pc": {"r": 146, "t": 259}, "movement": "c", "name": "Vagrant",},
         {"pc": {"r": 160, "t": 185}, "movement": "c", "name": "Balsamiq",}, 
@@ -135,6 +137,7 @@ var radar_data = [
         {"pc": {"r": 177, "t": 205}, "movement": "c", "name": "Optimal Workshop",}, 
         {"pc": {"r": 181, "t": 210}, "movement": "c", "name": "Zoom",}, 
         {"pc": {"r": 187, "t": 215}, "movement": "c", "name": "InVision App",}, 
+        {"pc": {"r": 192, "t": 251}, "movement": "t", "name": "Rancher",}, 
 
         //  Assess
         {"pc": {"r": 214, "t": 261}, "movement": "c", "name": "Chef",}, 
@@ -155,14 +158,14 @@ var radar_data = [
         {"pc": {"r": 279, "t": 253}, "movement": "c", "name": "Kubernetes",}, 
         {"pc": {"r": 281, "t": 210}, "movement": "c", "name": "Techsmith Morae",}, 
         {"pc": {"r": 283, "t": 228}, "movement": "c", "name": "Rocket.Chat",}, 
-        {"pc": {"r": 294, "t": 240}, "movement": "c", "name": "BaseCamp",}, 
-        {"pc": {"r": 294, "t": 259}, "movement": "c", "name": "Otto",},
+        {"pc": {"r": 294, "t": 240}, "movement": "c", "name": "BaseCamp",},
 
         //  Hold
         {"pc": {"r": 309, "t": 215}, "movement": "c", "name": "JIRA",}, 
         {"pc": {"r": 322, "t": 252}, "movement": "c", "name": "Habitat",}, 
+        {"pc": {"r": 388, "t": 259}, "movement": "t", "name": "Otto",},
         {"pc": {"r": 390, "t": 230}, "movement": "c", "name": "Nightwatch.js",}, 
-        {"pc": {"r": 399, "t": 220}, "movement": "c", "name": "TFVC",}, 
+        {"pc": {"r": 399, "t": 220}, "movement": "c", "name": "TFVC (except for CRM)",}, 
     ]
 }, {
     "quadrant": "Approaches", // t 270-360
@@ -185,9 +188,11 @@ var radar_data = [
         {"pc": {"r": 176, "t": 328}, "movement": "c", "name": "Eye Tracking",},
 
         //  Assess
+        {"pc": {"r": 228, "t": 335}, "movement": "t", "name": "VSTS for Release Management",},
         {"pc": {"r": 247, "t": 316}, "movement": "c", "name": "Reactive Architectures",}, 
         {"pc": {"r": 262, "t": 338}, "movement": "c", "name": "Backend for Front-Ends",}, 
         {"pc": {"r": 278, "t": 325}, "movement": "c", "name": "Model-Based Testing",},
+        {"pc": {"r": 238, "t": 345}, "movement": "t", "name": "CRM collaborative development without relying on TFVC",},
 
         //  Hold 
         {"pc": {"r": 358, "t": 277}, "movement": "c", "name": "Native Mobile",}, 
